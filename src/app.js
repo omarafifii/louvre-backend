df5e39818a835adfca136e909b28ce6efbdb897d
Omar Afifi <omarafifi@windowslive.com>
2021-06-23 17:34:05 +0200

    created jwt auth and set up swagger

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express')
-const userRouter = require('./routers/user')
+const userRouter = require('./routes/user')
 const port = process.env.PORT
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 require('./db/db')
 
 const app = express()
 
 app.use(express.json())
 app.use(userRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
